1d762091c57c217158a95ef54ab354bce2952149
Tom <1547107965@qq.com>
2022-07-24 15:45:04 +0800

    整理文档

---
+++ src/main/java/LearnBasic/box/Box.java
@@ -0,0 +1,16 @@
+package LearnBasic.box;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Box {
+    public static void main(String[] args) {
+        Integer a = 3;
+        Integer b = 3;
+        System.out.println(a == b);
+
+        Integer c = 300;
+        Integer d = 300;
+        System.out.println(c == d);
+    }
+}
